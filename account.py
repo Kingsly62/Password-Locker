9b256fa9d1f1472e83b7b7081701e31aaae3fc2d
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-01-23 10:38:24 +0300

    Add new class Accounts in the account.py file

---
+++ account.py
@@ -0,0 +1,6 @@
+class Accounts:
+    def __init__(self, firstname, lastname, email, username):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.username = username
