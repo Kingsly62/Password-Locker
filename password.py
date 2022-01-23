f094746f6eaafe6256be01625b932a241bb37fc7
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-01-23 15:52:45 +0300

    generate a new class for the password file to be Password

--- password.py
+++ password.py
@@ -1,14 +1,4 @@
-
-# import sys
-# PASSWORD = {'email': 'clinton56@gmail.com', 'Username': 'Kingsly62'}
-# if len(sys.argv) < 2:
-#     print('Usage: python3.9 password.py [account]- copy account password')
-#     sys.exit()
-#     account = sys.argv[1]
-#     import pyperclip
-
-#     if account in PASSWORD:
-#         pyperclip.copy(PASSWORD[account])
-#         print('password for ' + account + ' copied to clipboard')
-#     else:
-#         print('There is no account named ' + account)
+class Password:
+    '''
+    class of the password file
+    '''
