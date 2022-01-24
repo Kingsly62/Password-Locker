5a989326414d6a58c3b736a39e3cc0c516c90934
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-01-24 14:01:11 +0300

    Add new content in the file start.py

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 from account import Accounts
-from password import Password
+from password import Password, save_page
 
 
-def create_account(firstname, lastname, email, password):
-    account = Accounts(firstname, lastname, email, password)
+def create_account(firstname, lastname, username, email, password):
+    account = Accounts(firstname, lastname, username, email, password)
     return account
 
 
@@ -37,3 +37,79 @@ def main():
     while True:
         print(
             '1)Login\n 2) Register\n 3) About Passlock\n 4) Available Accounts\n 5) Logout')
+
+        choice = int(input())
+        if choice == 1:
+            print('Enter your  username')
+            username = input()
+            print('Enter your  password')
+            account = find_account(username)
+            if account.username == username and account.password == Password:
+                print('Already logged in')
+                while True:
+                    print(
+                        f'Welcome {username}, use the numbers below to select values')
+                    print(
+                        '1)Save new Password \n 2) Delete Password \n 3)Display saved password credentials \n 4)Logout')
+                    log_choice = int(input())
+                    if log_choice == 1:
+                        print('New Page')
+                        print('*'*100)
+
+                        print("Kindly provide your page name")
+                        page = input()
+                        print('Provide password')
+                        password = input()
+
+                        # save_page(create_page(page,password))
+                    elif log_choice == 2:
+                        print('Enter name to be erased')
+                        page = input()
+                        if isexist_page(page):
+                            remove_page = (page)
+                            delete_page(remove_page)
+
+                    elif log_choice == 3:
+                        if display_page():
+                            for pag in display_page():
+                                print(
+                                    f'{pag.page}:{pag.password}'
+                                )
+                            else:
+                                print('No password')
+                                print('/n')
+
+                        elif log_choice == 4:
+                            print('welcome again')
+                            break
+                        else:
+                            print('Wrong credentials provided')
+
+                    if choice == 2:
+                        print('New Account')
+                        print('*'*100)
+
+                        print('Enter first Name')
+                        firstname = input()
+
+                        print('Enter last Name')
+                        lastname = input()
+
+                        print('Enter username')
+                        username = input()
+
+                        print('Enter Preferred EMAIL')
+                        email = input()
+
+                        print('Enter passWord')
+                        password = input()
+
+                        save_account(create_account(
+                            firstname, lastname, username, email, password))
+
+                        print('SUCCESSFULLY CREATED ACCOUNT')
+                        while True:
+                            print(
+                                f'Welcome{username} Select the Numbers To Continue')
+                            print(
+                                f'1)Save Password \n 2)Delete password \n 3)Display saved password\n 4)Logout')
